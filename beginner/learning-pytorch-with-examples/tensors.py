19e5f1f46fadd759b882ef4ce8471252a118a0f3
ibaraujo <ibaraujo@deep02.vision.ime.usp.br>
2018-08-13 16:21:06 -0300

    Add the backpropagation step

--- beginner/learning-pytorch-with-examples/tensors.py
+++ beginner/learning-pytorch-with-examples/tensors.py
@@ -26,3 +26,15 @@ for t in range(500):
   # Compute and print loss
   loss = np.square(y_pred - y).sum()
   print(t, loss)
+
+  # Backprop to compute gradients of w1 and w2 with respect to loss
+  grad_y_pred = 2.0 * (y_pred - y)
+  grad_w2 = h_relu.T.dot(grad_y_pred)
+  grad_h_relu = grad_y_pred.dot(w2.T)
+  grad_h = grad_h_relu.copy()
+  grad_h[h < 0] = 0
+  grad_w1 = x.T.dot(grad_h)
+
+  # Update weights
+  w1 -= learning_rate * grad_w1
+  w2 -= learning_rate * grad_w2
