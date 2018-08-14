f4d1edf2c56304ddec0b4848857d5dbbffdca2a8
ibaraujo <ibaraujo@deep02.vision.ime.usp.br>
2018-08-14 15:49:51 -0300

    Fix terms

--- beginner/learning-pytorch-with-examples/tensors.py
+++ beginner/learning-pytorch-with-examples/tensors.py
@@ -52,10 +52,10 @@ device = torch.device("cuda:0")
 N, D_in, H, D_out = 64, 1000, 100, 10
 
 x = torch.randn(N, D_in, device=device, dtype=dtype)
-y = torch.randn(D, D_out, device=device, dtype=dtype)
+y = torch.randn(N, D_out, device=device, dtype=dtype)
 
-w1 = torch.randn(D, H, device=device, dtype=dtype)
-w2 = torch.randn(H, D_out, device=device, dtype=dtypea)
+w1 = torch.randn(D_in, H, device=device, dtype=dtype)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype)
 
 learning_rate = 1e-6
 for t in range(500):
