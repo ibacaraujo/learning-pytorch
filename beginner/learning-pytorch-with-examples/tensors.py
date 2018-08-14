d5befe796ef22c975b3923c4517a0435272279eb
ibaraujo <ibaraujo@deep02.vision.ime.usp.br>
2018-08-14 15:46:29 -0300

    Add backward step

--- beginner/learning-pytorch-with-examples/tensors.py
+++ beginner/learning-pytorch-with-examples/tensors.py
@@ -66,3 +66,12 @@ for t in range(500):
   loss = (y_pred - y).pow(2).sum().item()
   print(t, loss)
 
+  grad_y_pred = 2.0 * (y_pred - y)
+  grad_w2 = h_relu.t().mm(grad_y_pred)
+  grad_h_relu = grad_y_pred.mm(w2.t())
+  grad_h = grad_h_relu.clone()
+  grad_h[h < 0] = 0
+  grad_w1 = x.t().mm(grad_h)
+
+  w1 -= learning_rate * grad_w1
+  w2 -= learning_rate * grad_w2
