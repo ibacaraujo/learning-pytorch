d35432757ed8d985643782a7bf7b0afb94b7b30b
ibaraujo <ibaraujo@deep02.vision.ime.usp.br>
2018-08-20 17:43:35 -0300

    Add forward pass

--- beginner/learning-pytorch-with-examples/autograd.py
+++ beginner/learning-pytorch-with-examples/autograd.py
@@ -11,3 +11,9 @@ y = torch.randn(N, D_out, device=device, dtype=dtype)
 
 w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
 w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
+
+learning_rate = 1e-6
+for t in range(500):
+  y_pred = x.mm(w1).clamp(min=0).mm(w2)
+  loss = (y_pred - y).pow(2).sum()
+  print(t, loss.item())
