1235bc10d5556da392f508b5f558ccbc04a0cd62
ibaraujo <ibaraujo@deep02.vision.ime.usp.br>
2018-08-23 17:50:54 -0300

    Create TwoLayerNet

--- beginner/learning-pytorch-with-examples/nn_module.py
+++ beginner/learning-pytorch-with-examples/nn_module.py
@@ -44,3 +44,11 @@ for t in range(500):
   loss.backward()
 
   optimizer.step()
+
+# We can define our own Modules by subclassing nn.Module and defining a forward function
+
+class TwoLayerNet(torch.nn.Module):
+  def __init__(self, D_in, H, D_out):
+    super(TwoLayerNet, self).__init__()
+    self.linear1 = torch.nn.Linear(D_in, H)
+    self.linear2 = torch.nn.Linear(H, D_out)
