e7f2ebe7ff0e6d9083419eb1a328aeac30ae9672
ibaraujo <ibaraujo@deep02.vision.ime.usp.br>
2018-08-23 17:52:58 -0300

    Add forward function to the TwoLayerNet

--- beginner/learning-pytorch-with-examples/nn_module.py
+++ beginner/learning-pytorch-with-examples/nn_module.py
@@ -52,3 +52,8 @@ class TwoLayerNet(torch.nn.Module):
     super(TwoLayerNet, self).__init__()
     self.linear1 = torch.nn.Linear(D_in, H)
     self.linear2 = torch.nn.Linear(H, D_out)
+
+  def forward(self, x):
+    h_relu = self.linear1(x).clamp(min=0)
+    y_pred = self.linear2(h_relu)
+    return y_pred
