9f4bdfe3b2602cb41de8c99cd0e266f72b6fda96
Iago Breno <ibacaraujo@gmail.com>
2018-11-01 19:29:07 -0300

    Configure inputs

--- image/finetuning_torchvision_models.py
+++ image/finetuning_torchvision_models.py
@@ -1,3 +1,5 @@
+# Import libraries and functions
+
 from __future__ import print_function
 from __future__ import division
 import torch
@@ -12,3 +14,24 @@ import os
 import copy
 print("PyTorch Version: ",torch.__version__)
 print("Torchvision Version: ",torchvision.__version__)
+
+# Inputs
+
+# Directory conforms to the ImageFolder structure
+data_dir = "./hymenoptera_data"
+
+# Model chosen from [resnet, alexnet, vgg, squeezenet, densenet, inception]
+model_name = "squeezenet"
+
+# Number of classes in the dataset
+num_classes = 2
+
+# Batch size for training
+batch_size = 8
+
+# Number of epochs to train for
+num_epochs = 15
+
+# Flag for feature extracting. When False, we finetune the whole model.
+# If True, we finetune only the reshaped layer params
+feature_extract = True
