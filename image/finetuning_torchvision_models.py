3cea6d56a291174bf3a73dda8face45f893e2b3a
Iago Breno <ibacaraujo@gmail.com>
2018-11-03 22:45:20 -0300

    Initialize variables

--- image/finetuning_torchvision_models.py
+++ image/finetuning_torchvision_models.py
@@ -117,4 +117,7 @@ def set_parameter_requires_grad(model, feature_extracting):
             param.requires_grad = False
            
 def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
-    pass
+     # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0
