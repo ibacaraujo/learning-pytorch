e23b10f299090afc8c59a9e9ba2c3ad355ba4ee8
Iago Breno <ibacaraujo@gmail.com>
2018-11-05 18:33:07 -0200

    Plot learning curves

--- image/finetuning_torchvision_models.py
+++ image/finetuning_torchvision_models.py
@@ -257,3 +257,22 @@ scratch_optimizer = optim.SGD(scratch_model.parameters(), lr=0.001, momentum=0.9
 scratch_criterion = nn.CrossEntropyLoss()
 _,scratch_hist = train_model(scratch_model, dataloaders_dict, scratch_criterion, scratch_optimizer, 
                              num_epochs=num_epochs, is_inception=(model_name=="inception"))
+
+# Plot the training curves of validation accuracy vs. number
+#  of training epochs for the transfer learning method and
+#  the model trained from scratch
+ohist = []
+shist = []
+
+ohist = [h.cpu().numpy() for h in hist]
+shist = [h.cpu().numpy() for h in scratch_hist]
+
+plt.title("Validation Accuracy vs. Number of Training Epochs")
+plt.xlabel("Training Epochs")
+plt.ylabel("Validation Accuracy")
+plt.plot(range(1,num_epochs+1),ohist,label="Pretrained")
+plt.plot(range(1,num_epochs+1),shist,label="Scratch")
+plt.ylim((0,1.))
+plt.xticks(np.arange(1, num_epochs+1, 1.0))
+plt.legend()
+plt.show()
