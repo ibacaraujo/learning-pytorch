68e2e086b134bf1fc82f1e79cc40bc57a196145d
Iago Breno <ibacaraujo@gmail.com>
2018-11-05 18:15:30 -0200

    Create optimizer

--- image/finetuning_torchvision_models.py
+++ image/finetuning_torchvision_models.py
@@ -218,3 +218,23 @@ dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size
 
 # Detect if we have a GPU available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Create the optimizer
+
+# Send the model to GPU
+model_ft = model_ft.to(device)
+params_to_update = model_ft.parameters()
+print("Params to learn:")
+if feature_extract:
+    params_to_update = []
+    for name,param in model_ft.named_parameters():
+        if param.requires_grad == True:
+            params_to_update.append(param)
+            print("\t",name)
+else:
+    for name,param in model_ft.named_parameters():
+        if param.requires_grad == True:
+            print("\t",name)
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
